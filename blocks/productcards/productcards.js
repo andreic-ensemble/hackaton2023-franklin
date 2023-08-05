8ead4119756b8cff24c4276cc9edda3e9dca4aae
andrei.curta <andrei.curta@ensemble.com>
2023-08-05 21:48:30 +0300

    update productcards

--- blocks/productcards/productcards.js
+++ blocks/productcards/productcards.js
@@ -1,15 +1,19 @@
 import { createOptimizedPicture } from '../../scripts/lib-franklin.js';
 
-export default function decorate(block) {
+const processRow = (div, index) => {
+  if (div.children.length === 1 && div.querySelector('picture')) div.className = 'productcards-card-image';
+  else if(index === 2) div.className = 'productcards-card-price'
+  else div.className = 'productcards-card-body';
+}
 
+export default function decorate(block) {
   /* change to ul, li */
   const ul = document.createElement('ul');
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
     while (row.firstElementChild) li.append(row.firstElementChild);
-    [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'productcards-card-image';
-      else div.className = 'productcards-card-body';
+    [...li.children].forEach((div, index) => { processRow(div, index)
+
     });
     ul.append(li);
   });
